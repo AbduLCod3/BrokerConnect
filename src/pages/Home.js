c86cf7fef12ddb2b5d87c4eb975fd6f320e86a9e
Abdul R <abdul.kasmo.iv@outlook.com>
2023-04-17 22:38:46 +0200

    add css code, update home page file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <h4>This is the Home page</h4>;
+  return (
+    <div className="HomePage">
+      <h4>This is the Home page</h4>
+      <div className="HomePageLeftDiv">
+        <h1>Let Us Help You Connect To Your Future Clients</h1>
+      </div>
+      <div className="HomePageRightDiv">
+        {" "}
+        <img
+          className="img1"
+          src={require("../images/HouseImage.jpg")}
+          alt="imgHouse"
+        />
+      </div>
+    </div>
+  );
 }
